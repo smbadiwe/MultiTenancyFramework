50a14d106e30a57e40b90a6d17cbc32f31e09ffd
smbadiwe <somasystemsng@gmail.com>
2016-09-09 18:28:39 -0700

    URL now fully lowercase. Constraint on {id} removed

--- MVC5/MultiTenancyFramework.Mvc/MvcUtils/InstitutionRouteConfig.cs
+++ MVC5/MultiTenancyFramework.Mvc/MvcUtils/InstitutionRouteConfig.cs
@@ -12,7 +12,9 @@ namespace MultiTenancyFramework.Mvc
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             routes.IgnoreRoute("{*staticfile}", new { staticfile = @".*\.(css|less|sass|js|gif|png|jpg|jpeg|ico|svg|ttf|eot\woff|woff2|xml|csv|txt|map|json)(/.*)?" });
-          
+
+            routes.LowercaseUrls = true;
+
             routes.MapMvcAttributeRoutes();
 
             // From most specific to most general
@@ -20,50 +22,50 @@ namespace MultiTenancyFramework.Mvc
             routes.MapRoute(
                 name: "Default",
                 url: "",
-                defaults: new { institution = Utilities.INST_DEFAULT_CODE, area = "", controller = "Home", action = "Index", id = UrlParameter.Optional },
-                constraints: new { id = @"\d*" }
+                defaults: new { institution = Utilities.INST_DEFAULT_CODE, area = "", controller = "Home", action = "Index", id = UrlParameter.Optional } //,
+                //constraints: new { id = @"\d*" }
             );
 
             routes.MapRoute(
                 name: "Error",
                 url: "Error",
-                defaults: new { institution = Utilities.INST_DEFAULT_CODE, area = "", controller = "Error", action = "Index", id = UrlParameter.Optional },
-                constraints: new { id = @"\d*" }
+                defaults: new { institution = Utilities.INST_DEFAULT_CODE, area = "", controller = "Error", action = "Index", id = UrlParameter.Optional } //,
+                //constraints: new { id = @"\d*" }
             );
 
             routes.MapRoute(
                 name: "TenantError",
                 url: "{institution}/Error",
-                defaults: new { area = "", controller = "Error", action = "Index", id = UrlParameter.Optional },
-                constraints: new { id = @"\d*" }
+                defaults: new { area = "", controller = "Error", action = "Index", id = UrlParameter.Optional } //,
+                //constraints: new { id = @"\d*" }
             );
 
             routes.MapRoute(
                 name: "InstitutionOnly",
                 url: "{institution}",
-                defaults: new { area = "", controller = "Home", action = "Index", id = UrlParameter.Optional },
-                constraints: new { id = @"\d*" }
+                defaults: new { area = "", controller = "Home", action = "Index", id = UrlParameter.Optional } //,
+                //constraints: new { id = @"\d*" }
             );
 
             routes.MapRoute(
                 name: "ControllerAndActionOnly",
                 url: "{controller}/{action}/{id}",
-                defaults: new { institution = Utilities.INST_DEFAULT_CODE, area = "", id = UrlParameter.Optional },
-                constraints: new { id = @"\d*" }
+                defaults: new { institution = Utilities.INST_DEFAULT_CODE, area = "", id = UrlParameter.Optional } //,
+                //constraints: new { id = @"\d*" }
             );
 
             routes.MapRoute(
                 name: "MultiTenant",
                 url: "{institution}/{controller}/{action}/{id}",
-                defaults: new { area = "", id = UrlParameter.Optional },
-                constraints: new { id = @"\d*" }
+                defaults: new { area = "", id = UrlParameter.Optional } //,
+                //constraints: new { id = @"\d*" }
             );
 
             routes.MapRoute(
                 name: "MultiTenantWithArea",
                 url: "{institution}/{area}/{controller}/{action}/{id}",
-                defaults: new { id = UrlParameter.Optional },
-                constraints: new { id = @"\d*" }
+                defaults: new { id = UrlParameter.Optional } //, 
+                //constraints: new { id = @"\d*" }
             );
 
         }
