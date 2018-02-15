4afc184412d8150ee4a43cbf783446f344511b96
Somadina Mbadiwe <smbadiwe@github.com>
2018-02-15 03:54:04 -0800

    Added a get for the tenant ids

--- MultiTenancyFramework.Core/Entities/DatabaseConnection.cs
+++ MultiTenancyFramework.Core/Entities/DatabaseConnection.cs
@@ -1,12 +1,37 @@
-﻿namespace MultiTenancyFramework.Entities
+﻿using System;
+using System.Collections.Generic;
+
+namespace MultiTenancyFramework.Entities
 {
     public class DatabaseConnection : Entity, IAmHostedCentrally
     {
         public virtual string ConnectionString { get; set; }
         /// <summary>
-        /// Comma-separated values of the InstitutionCode of the tenants whose data is on this DB
+        /// Comma-separated values of the IDs of the tenants whose data is on this DB
         /// </summary>
         public virtual string TenantsOnIt { get; set; }
+        /// <summary>
+        /// The IDs of the tenants whose data is on this DB
+        /// </summary>
+        public virtual HashSet<long> TenantsIDs
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(TenantsOnIt)) return new HashSet<long>();
+
+                var splitted = TenantsOnIt.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                HashSet<long> userRoles = new HashSet<long>();
+                foreach (var item in splitted)
+                {
+                    long id;
+                    if (long.TryParse(item, out id))
+                    {
+                        userRoles.Add(id);
+                    }
+                }
+                return userRoles;
+            }
+        }
         public virtual int MaximumNumberOfInstitutionsHosted { get; set; } = 1;
         public virtual int NumberOfInstitutionsCurrentlyHosted { get; set; } = 1;
     }
