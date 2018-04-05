a25762dd21f1c4bee7a53462319baf99bca2f329
Somadina Mbadiwe <smbadiwe@github.com>
2018-04-05 02:40:49 -0700

    Added the action for keep-alive task.

---
+++ MVC5/MultiTenancyFramework.Mvc/MvcUtils/KeepAliveController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace MultiTenancyFramework.Mvc
+{
+    /// <summary>
+    /// A scheduled task calls this keepalive/index to, you know, keep the app alive.
+    /// </summary>
+    /// <seealso cref="Controller" />
+    [AllowAnonymous]
+    public class KeepAliveController : Controller
+    {
+        public virtual ActionResult Index()
+        {
+            return new EmptyResult();
+        }
+    }
+}
